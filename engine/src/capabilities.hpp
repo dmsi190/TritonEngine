a78c46809a9d11f1452acc91a87fae896a942adb
Dmitry Sirachenko <dmitry.sirachenko@bestway.com.ua>
2026-02-01 12:30:04 +0200

    Add new hash table capability values

--- engine/src/capabilities.hpp
+++ engine/src/capabilities.hpp
@@ -23,5 +23,8 @@ namespace triton
         types::usize maxRenderTextureAtlasTextureCount = 8192;
         types::usize vertexBufferSize = 64 * 1024 * 1024;
         types::usize indexBufferSize = 64 * 1024 * 1024;
+        types::usize hashTableChunkByteSize = 16 * 1024;
+        types::usize hashTableMaxChunkCount = 256;
+        types::usize hashTableSize = 4096;
     };
 }
