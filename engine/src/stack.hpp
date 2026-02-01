75b92e9d6689c68bf1e7c922e59dd4a9aee86608
Dmitry Sirachenko <dmitry.sirachenko@bestway.com.ua>
2026-02-01 17:36:40 +0200

    Add cStack data structure

---
+++ engine/src/stack.hpp
@@ -0,0 +1,247 @@
+// stack.hpp
+
+#pragma once
+
+#include <type_traits>
+#include "object.hpp"
+#include "context.hpp"
+#include "memory_pool.hpp"
+#include "types.hpp"
+
+namespace triton
+{
+	template <typename TValue>
+	class cStack;
+
+	class cStackValue
+	{
+		template <typename>
+		friend class cStack;
+
+		types::u32 chunk = 0;
+		types::u32 localPosition = 0;
+		types::u32 globalPosition = 0;
+	};
+
+	struct sChunkAllocatorDescriptor
+	{
+		types::usize chunkByteSize = 16 * 1024;
+		types::usize maxChunkCount = 256;
+		types::usize hashTableSize = 4096;
+	};
+
+	template <typename TValue>
+	class cStack : public iObject
+	{
+	public:
+		static_assert(std::is_base_of_v<cStackValue, TValue>, "TValue must inherit from cStackValue");
+
+		explicit cStack(cContext* context, const sChunkAllocatorDescriptor& allocatorDesc);
+		virtual ~cStack() override final;
+
+		template<typename... Args>
+		TValue* Push(Args&&... args);
+		TValue* Push(TValue&& value);
+		TValue* At(types::u32 index) const;
+		TValue* At(const cStackValue& value) const;
+		TValue* Top() const;
+		void Erase(types::u32 index);
+		void Pop();
+
+		inline types::usize GetSize() const { return _elementCount; }
+
+	private:
+		cStackValue New();
+		types::u32 AllocateChunk();
+		void DeallocateChunk(types::u32 chunkIndex);
+		types::u32 GetChunkIndex(types::u32 globalPosition);
+		types::u32 GetChunkLocalPosition(types::u32 chunkIndex, types::u32 globalPosition);
+		types::cpuword MakeHashMask(types::usize size);
+
+	private:
+		sChunkAllocatorDescriptor _allocatorDesc = {};
+		types::usize _chunkCount = 0;
+		types::usize _objectByteSize = 0;
+		types::usize _objectCountPerChunk = 0;
+		types::usize _elementCount = 0;
+		TValue** _chunks = nullptr;
+	};
+
+	template <typename TValue>
+	cStack<TValue>::cStack(cContext* context, const sChunkAllocatorDescriptor& allocatorDesc) : iObject(context)
+	{
+		const sCapabilities* caps = _context->GetSubsystem<cEngine>()->GetApplication()->GetCapabilities();
+		cMemoryAllocator* memoryAllocator = _context->GetMemoryAllocator();
+
+		_allocatorDesc = allocatorDesc;
+		_objectByteSize = sizeof(TValue);
+		_objectCountPerChunk = _allocatorDesc.chunkByteSize / _objectByteSize;
+		_chunks = (TValue**)memoryAllocator->Allocate(_allocatorDesc.maxChunkCount * sizeof(TValue*), caps->memoryAlignment);
+
+		AllocateChunk();
+	}
+
+	template <typename TValue>
+	cStack<TValue>::~cStack()
+	{
+		for (types::usize i = 0; i < _elementCount; i++)
+			Pop();
+
+		cMemoryAllocator* memoryAllocator = _context->GetMemoryAllocator();
+		memoryAllocator->Deallocate(_chunks);
+	}
+
+	template <typename TValue>
+	template <typename... Args>
+	TValue* cStack<TValue>::Push(Args&&... args)
+	{
+		cStackValue se = New();
+
+		TValue* object = _context->Create<TValue>(_chunks[se.chunk], se.localPosition, std::forward<Args>(args)...);
+		object->chunk = se.chunk;
+		object->localPosition = se.localPosition;
+		object->globalPosition = se.globalPosition;
+
+		return object;
+	}
+
+	template <typename TValue>
+	TValue* cStack<TValue>::Push(TValue&& value)
+	{
+		cStackValue se = New();
+
+		_chunks[se.chunk][se.localPosition] = std::move(value);
+
+		TValue* object = &_chunks[se.chunk][se.localPosition];
+		object->chunk = se.chunk;
+		object->localPosition = se.localPosition;
+		object->globalPosition = se.globalPosition;
+
+		return object;
+	}
+
+	template <typename TValue>
+	TValue* cStack<TValue>::At(types::u32 index) const
+	{
+		if (_chunkCount == 0 || index >= _elementCount)
+			return nullptr;
+
+		const types::u32 chunkIndex = GetChunkIndex(index);
+		const types::u32 localPosition = GetChunkLocalPosition(chunkIndex, index);
+
+		return &_chunks[chunkIndex][localPosition];
+	}
+
+	template <typename TValue>
+	TValue* cStack<TValue>::At(const cStackValue& value) const
+	{
+		return At(value.globalPosition);
+	}
+
+	template <typename TValue>
+	TValue* cStack<TValue>::Top() const
+	{
+		if (_elementCount == 0)
+			return nullptr;
+
+		return At(_elementCount - 1);
+	}
+
+	template <typename TValue>
+	void cStack<TValue>::Erase(types::u32 index)
+	{
+		if (_elementCount == 0 || index >= _elementCount)
+			return;
+
+		const types::u32 lastChunkIndex = _chunkCount - 1;
+		const types::u32 chunkIndex = GetChunkIndex(index);
+		const types::usize lastChunkObjectCount = GetChunkLocalPosition(lastChunkIndex, _elementCount);
+		const types::u32 lastLocalPosition = lastChunkObjectCount - 1;
+		const types::usize localPosition = GetChunkLocalPosition(chunkIndex, index);
+		const types::boolean isLastChunk = chunkIndex == lastChunkIndex;
+
+		if (chunkIndex >= _chunkCount || (isLastChunk == types::K_TRUE && localPosition >= lastChunkObjectCount))
+			return;
+
+		_chunks[chunkIndex][localPosition].~TValue();
+		_chunks[chunkIndex][localPosition] = _chunks[lastChunkIndex][lastLocalPosition];
+		_elementCount -= 1;
+
+		if (lastChunkObjectCount == 1)
+			DeallocateChunk(lastChunkIndex);
+	}
+
+	template <typename TValue>
+	void cStack<TValue>::Pop()
+	{
+		Erase(_elementCount - 1);
+	}
+
+	template <typename TValue>
+	cStackValue cStack<TValue>::New()
+	{
+		types::u32 elementChunkIndex = GetChunkIndex(_elementCount);
+		types::u32 elementLocalPosition = GetChunkLocalPosition(elementChunkIndex, _elementCount);
+
+		const types::usize chunkObjectCount = GetChunkLocalPosition(_chunkCount - 1, _elementCount);
+		if (chunkObjectCount >= _objectCountPerChunk)
+		{
+			elementChunkIndex = AllocateChunk();
+			elementLocalPosition = 0;
+		}
+
+		cStackValue se = {};
+		se.chunk = elementChunkIndex;
+		se.localPosition = elementLocalPosition;
+		se.globalPosition = _elementCount++;
+
+		return se;
+	}
+
+	template <typename TValue>
+	types::u32 cStack<TValue>::AllocateChunk()
+	{
+		if (_chunkCount >= _maxChunkCount)
+			return 0;
+
+		const sCapabilities* caps = _context->GetSubsystem<cEngine>()->GetApplication()->GetCapabilities();
+		cMemoryAllocator* memoryAllocator = _context->GetMemoryAllocator();
+		_chunks[_chunkCount] = (TValue*)memoryAllocator->Allocate(_chunkByteSize, caps->memoryAlignment);
+
+		return _chunkCount++;
+	}
+
+	template <typename TValue>
+	void cStack<TValue>::DeallocateChunk(types::u32 chunkIndex)
+	{
+		if (chunkIndex >= _chunkCount--)
+			return;
+
+		cMemoryAllocator* memoryAllocator = _context->GetMemoryAllocator();
+		memoryAllocator->Deallocate(_chunks[chunkIndex]);
+	}
+
+	template <typename TValue>
+	types::u32 cStack<TValue>::GetChunkIndex(types::u32 globalPosition)
+	{
+		return globalPosition / _objectCountPerChunk;
+	}
+
+	template <typename TValue>
+	types::u32 cStack<TValue>::GetChunkLocalPosition(types::u32 chunkIndex, types::u32 globalPosition)
+	{
+		const types::u32 chunkIndexBoundary = chunkIndex * _objectCountPerChunk;
+		const types::u32 localPosition = globalPosition - chunkIndexBoundary;
+
+		return localPosition;
+	}
+
+	template <typename TValue>
+	types::cpuword cStack<TValue>::MakeHashMask(types::usize size)
+	{
+		unsigned int count = __lzcnt((unsigned int)size);
+		types::cpuword mask = (types::cpuword)((1 << (31 - count)) - 1);
+
+		return mask;
+	}
+}
