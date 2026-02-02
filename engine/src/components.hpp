78a9fd8eb6ab6c222c32bb960217c4237c4eedd9
Dmitry Sirachenko <dmitry.sirachenko@bestway.com.ua>
2026-02-02 16:05:25 +0200

    Add sCameraComponents to "components.hpp"

--- engine/src/components.hpp
+++ engine/src/components.hpp
@@ -1,5 +1,16 @@
 #pragma once
 
+#include "math.hpp"
+
 namespace triton::ecs::components
 {
+	struct sCameraComponent
+	{
+        triton::cMatrix4 _view = triton::cMatrix4(1.0f);
+        triton::cMatrix4 _projection = triton::cMatrix4(1.0f);
+        triton::cMatrix4 _viewProjection = triton::cMatrix4(1.0f);
+        types::f32 _fov = 60.0f;
+        types::f32 _zNear = 0.01f;
+        types::f32 _zFar = 100.0f;
+	};
 }
