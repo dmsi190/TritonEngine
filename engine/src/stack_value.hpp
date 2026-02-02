8d210c648d2c14ee0abe0eba9efbdca18c23030c
Dmitry Sirachenko <dmitry.sirachenko@bestway.com.ua>
2026-02-02 21:47:54 +0200

    Remove compile errors from "stack_value.hpp"

--- engine/src/stack_value.hpp
+++ engine/src/stack_value.hpp
@@ -6,11 +6,15 @@ namespace triton
 {
 	template <typename TValue>
 	class cStack;
+	template <typename TKey, typename TValue>
+	class cHashTable;
 
 	class cStackValue
 	{
 		template <typename>
 		friend class cStack;
+		template <typename, typename>
+		friend class cHashTable;
 
 		types::u32 chunk = 0;
 		types::u32 localPosition = 0;
